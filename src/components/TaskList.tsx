b5a4f22f47a3975877dadeb5951c001b8cb30819
Lucas Carrias <lucascarrias@outlook.com>
2021-05-04 17:32:37 -0300

    Add a new task that has a title

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,7 +15,17 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle.length > 0) {
+      
+      const newTask = {
+        id: Date.now(),
+        title: newTaskTitle,
+        isComplete: false
+      }
+
+      setTasks([...tasks, newTask])
+      setNewTaskTitle('');
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
